d9f3d40339d65d63453197563e323a5c826849e5
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-05-11 20:07:41 -0700

    Removing unused assets final touches

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
@@ -12,16 +13,24 @@ public class Ghost : MonoBehaviour
     public static string PickedLvl = "";
     public bool amOpening;
 
+    public GameObject carSelector;
+    public GameObject lvlSelector;
+    
     private bool m_HasPickedCar;
     private bool m_HasPickedLvl;
     
     private void Start()
     {
-        if (amOpening) return;
-        Debug.Log("You're going to use car " + PickedCar);
+        Application.targetFrameRate = -1;
+        QualitySettings.vSyncCount = 0;
+        
+        if (!amOpening) return;
+        carSelector = GameObject.FindGameObjectWithTag("carLight");
+        lvlSelector = GameObject.FindGameObjectWithTag("lvlLight");
+
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0) && amOpening)
         {
@@ -30,23 +39,27 @@ public class Ghost : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Car"))
                 {
-                    Debug.Log("You've chosen the car " + hit.collider.gameObject.name);
+                    // Debug.Log("You've chosen the car " + hit.collider.gameObject.name);
                     PickedCar = hit.collider.gameObject.name;
                     m_HasPickedCar = true;
+                    var position = hit.collider.transform.position;
+                    carSelector.transform.position = new Vector3(position.x,10f, position.z);
                 }
 
                 if (hit.collider.CompareTag("Level"))
                 {
-                    Debug.Log("You've chosen the level " + hit.collider.name);
+                    // Debug.Log("You've chosen the level " + hit.collider.name);
                     PickedLvl = hit.collider.gameObject.name;
                     m_HasPickedLvl = true;
+                    var position = hit.collider.transform.position;
+                    lvlSelector.transform.position = new Vector3(position.x,10f, position.z);
                 }
 
             }
         }
 
         if (!m_HasPickedCar || !m_HasPickedLvl) return;
-        Debug.Log("With car " + PickedCar + " and level " + PickedLvl + " starting game");
+        // Debug.Log("With car " + PickedCar + " and level " + PickedLvl + " starting game");
         SceneManager.LoadScene(PickedLvl);
 
     }
